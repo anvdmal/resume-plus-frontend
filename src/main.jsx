ef19c7d27a191b4a4956827e208d089a306e458d
vvikobra <viktoria.obrokova@yandex.ru>
2025-10-17 16:19:57 +0300

    fix: import color variable

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import './variables/typography.css';
+import './variables/colors.css';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
