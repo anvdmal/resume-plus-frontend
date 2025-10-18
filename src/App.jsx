2b9858405e11c09e873e1b4fd5635a75307f89a2
“Nastya” <nastya.cap04@gmail.com>
2025-10-19 02:53:55 +0300

    add ResumeForm file with styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import ResumeForm from './resume-form/ResumeForm';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  return <ResumeForm />;
 
-  return (
+  /* return (
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
@@ -29,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  ) */
 }
 
 export default App
