eaa1174405321ee7ecdb54545555fb52e76d7f3c
vvikobra <viktoria.obrokova@yandex.ru>
2025-10-21 11:38:25 +0300

    fix App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { useState, useRef, useEffect } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import Sidebar from './components/Sidebar';
 import './App.css';
 
 function App() {
-    const [count, setCount] = useState(0);
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
     const pdfInputRef = useRef(null);
     const jsonInputRef = useRef(null);
@@ -65,15 +62,6 @@ function App() {
                     ☰
                 </button>
 
-                <div className="logo-row">
-                    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-                        <img src={viteLogo} className="logo" alt="Vite logo" />
-                    </a>
-                    <a href="https://react.dev" target="_blank" rel="noreferrer">
-                        <img src={reactLogo} className="logo react" alt="React logo" />
-                    </a>
-                </div>
-
                 <h1>Resume+</h1>
 
                 <div className="import-buttons">
@@ -101,18 +89,7 @@ function App() {
                     onChange={onJSONFileChange}
                 />
 
-                <div className="card">
-                    <button onClick={() => setCount((count) => count + 1)}>
-                        count is {count}
-                    </button>
-                    <p>
-                        Edit <code>src/App.jsx</code> and save to test HMR
-                    </p>
-                </div>
 
-                <p className="read-the-docs">
-                    Click on the Vite and React logos to learn more
-                </p>
             </main>
         </>
     );
