d45d1bb7e8555de8b91db2c79b51e8c73be572d5
vvikobra <viktoria.obrokova@yandex.ru>
2025-10-26 17:29:25 +0300

    fix resume preview position

--- src/components/form/sections/PersonalInfoSection.jsx
+++ src/components/form/sections/PersonalInfoSection.jsx
@@ -1,60 +1,71 @@
 import React from 'react';
-import { Controller } from 'react-hook-form';
+import {Controller} from 'react-hook-form';
 import InputField from '../../ui/InputField.jsx';
 import PhotoUpload from '../PhotoUpload.jsx';
 import SectionHeader from '../SectionHeader.jsx';
 import DatePickerField from '../DatePickerField.jsx';
 import '../../../styles/PersonalInfoSection.css';
 
-export default function PersonalInfoSection({ control }) {
-  return (
-    <section className="personal-info-section section">
-      <SectionHeader 
-        iconSrc="src/assets/ic-personal-data.svg" 
-        title="Личные данные"
-      />
-      <div className="personal-info-layout">
-        <div className="photo-column">
-          <PhotoUpload name="photo" control={control} />
-        </div>
-        <div className="fields-column">
-          <div className="group-column">
-            <Controller
-              name="firstName"
-              control={control}
-              render={({ field }) => <InputField placeholder="Имя" variant="gray" {...field} />}
+export default function PersonalInfoSection({control, setValue, setPhotoPreview}) {
+    return (
+        <section className="personal-info-section section">
+            <SectionHeader
+                iconSrc="src/assets/ic-personal-data.svg"
+                title="Личные данные"
             />
-            <Controller
-              name="lastName"
-              control={control}
-              render={({ field }) => <InputField placeholder="Фамилия" variant="gray" {...field} />}
-            />
-            <Controller
-              name="middleName"
-              control={control}
-              render={({ field }) => <InputField placeholder="Отчество" variant="gray" {...field} />}
-            />
-          </div>
-          <div className="group-column">
-            <DatePickerField
-              name="birthDate"
-              control={control}
-              placeholderText="Дата рождения"
-              maxDate={new Date()}
-            />
-            <Controller
-              name="country"
-              control={control}
-              render={({ field }) => <InputField placeholder="Страна" variant="gray" {...field} />}
-            />
-            <Controller
-              name="city"
-              control={control}
-              render={({ field }) => <InputField placeholder="Город" variant="gray" {...field} />}
-            />
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+            <div className="personal-info-layout">
+                <div className="photo-column">
+                    <Controller
+                        name="photo"
+                        control={control}
+                        render={({field}) => (
+                            <PhotoUpload
+                                name="photo"
+                                value={field.value}
+                                setValue={setValue}
+                                setPhotoPreview={setPhotoPreview}
+                            />
+                        )}
+                    />
+                </div>
+                <div className="fields-column">
+                    <div className="group-column">
+                        <Controller
+                            name="firstName"
+                            control={control}
+                            render={({field}) => <InputField placeholder="Имя" variant="gray" {...field} />}
+                        />
+                        <Controller
+                            name="lastName"
+                            control={control}
+                            render={({field}) => <InputField placeholder="Фамилия" variant="gray" {...field} />}
+                        />
+                        <Controller
+                            name="middleName"
+                            control={control}
+                            render={({field}) => <InputField placeholder="Отчество" variant="gray" {...field} />}
+                        />
+                    </div>
+                    <div className="group-column">
+                        <DatePickerField
+                            name="birthDate"
+                            control={control}
+                            placeholderText="Дата рождения"
+                            maxDate={new Date()}
+                        />
+                        <Controller
+                            name="country"
+                            control={control}
+                            render={({field}) => <InputField placeholder="Страна" variant="gray" {...field} />}
+                        />
+                        <Controller
+                            name="city"
+                            control={control}
+                            render={({field}) => <InputField placeholder="Город" variant="gray" {...field} />}
+                        />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
