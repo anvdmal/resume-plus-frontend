04a1b84e1498c547e5bae49a5072a8f7ede17efc
anvdmal <alekseeva.ann04@mail.ru>
2025-10-29 22:32:06 +0300

    add resume examples section

---
+++ src/components/TemplatesPreviewSection.jsx
@@ -0,0 +1,23 @@
+import Template1 from "../assets/img-template-one.png";
+import Template2 from "../assets/img-template-one.png"; //заменить на готовые шаблоны
+import Template3 from "../assets/img-template-one.png";
+import "../styles/TemplatesPreviewSection.css";
+
+export default function TemplatesPreviewSection() {
+    return (
+        <div className="templates-preview-section">
+            <p className={"section-title"}>Примеры резюме</p>
+            <div className="templates-container">
+                <div className="template-card">
+                    <img src={Template1} alt="Шаблон резюме 1"/>
+                </div>
+                <div className="template-card">
+                    <img src={Template2} alt="Шаблон резюме 2"/>
+                </div>
+                <div className="template-card">
+                    <img src={Template3} alt="Шаблон резюме 3"/>
+                </div>
+            </div>
+        </div>
+    );
+}
