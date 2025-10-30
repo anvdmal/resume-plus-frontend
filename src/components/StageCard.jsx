160e87d1d2c3995c3e273c08af1d921311b6c35d
anvdmal <alekseeva.ann04@mail.ru>
2025-10-30 21:00:14 +0300

    change class name for title in stage card

--- src/components/StageCard.jsx
+++ src/components/StageCard.jsx
@@ -5,7 +5,7 @@ export default function StageCard({icon, title, description}) {
     return(
         <div className={"stage-card"}>
             <img className={"icon"} src={icon} alt={"stage icon"}/>
-            <p className={"title"}>{title}</p>
+            <p className={"stage-title"}>{title}</p>
             <p className={"description"}>{description}</p>
         </div>
     )
