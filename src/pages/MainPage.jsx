308919bd79d80cdffb70a75aa4142ccfdf63657a
anvdmal <alekseeva.ann04@mail.ru>
2025-10-30 21:02:52 +0300

    add main page with style

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,18 +1,21 @@
+import React from "react";
 import Header from "../components/Header.jsx";
 import LetsCreateResumeSection from "../components/main_sections/LetsCreateResumeSection.jsx";
-import TemplatesPreviewSection from "../components/main_sections/TemplatesPreviewSection.jsx";
 import AiResumeSection from "../components/main_sections/AiResumeSection.jsx";
-import React from "react";
 import CreationStagesSection from "../components/main_sections/CreationStagesSection.jsx";
+import TemplatesPreviewSection from "../components/main_sections/TemplatesPreviewSection.jsx";
+import "../styles/MainPage.css"
+import Footer from "../components/Footer.jsx";
 
 export default function MainPage() {
     return (
-        <div>
+        <div className={"main-page"}>
             <Header variant="main-screen"/>
             <LetsCreateResumeSection/>
             <AiResumeSection />
             <TemplatesPreviewSection/>
             <CreationStagesSection/>
+            <Footer/>
         </div>
     )
 }
