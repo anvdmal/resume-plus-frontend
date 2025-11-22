2eaf850acb89ac81724d4150f61f3b93e1521e11
vvikobra <viktoria.obrokova@yandex.ru>
2025-11-22 16:05:19 +0300

    fix modern template preview

--- src/pages/ResumeEditorPage.jsx
+++ src/pages/ResumeEditorPage.jsx
@@ -33,7 +33,7 @@ export default function ResumeEditorPage() {
                     />
                 </div>
                 <div className="resume-edit-page-preview">
-                    <ResumeRenderer data={resumeData} template="creative"/>
+                    <ResumeRenderer data={resumeData} template="modern"/>
                 </div>
             </div>
             <Footer/>
